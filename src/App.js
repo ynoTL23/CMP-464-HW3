2d3e72200a98eb6493d0de67928cf7f8c9e65002
elwinJ <elwinjosue@gmail.com>
2021-04-19 20:19:24 -0400

    creating my own branch.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello to the world of dev branch</h1>
+      <h1>Hello to the world of josue branch</h1>
     </div>
   );
 }
