19b65e04291f70c14d7c9a35a6bdeaffe3ce4419
falilo <fadilusa217@gmail.com>
2021-04-21 21:27:23 -0400

    I create the PadInput where the pad buttoms will go in

---
+++ src/components/PadInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PadInput = () =>{
+   
+    return <canvas id="canvas" style={{backgroundColor: 'blue', outline:'5px solid black', marginLeft:'5%'}}
+                               
+     width='200' 
+     height='300' >
+         Canvas
+         </canvas>;
+
+}
+export default PadInput;
