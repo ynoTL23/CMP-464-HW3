ebdfb921e487bee8d2bf89c7759e380ee2a4ec70
sajia <sajia@gmail.com>
2021-04-24 08:42:50 +0530

    Designboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Header from './components/Header'
 import PadInput from './components/PadInput'
-import PadButton from './components/PadButton'
 import DesignBoard from './components/DesignBoard'
     
 import './App.css'
@@ -11,17 +10,10 @@ import './styles/style.css'
 function App() {
   return (
     <div className='App'>
-      <h1>Hello to the world of dev branch</h1>
       <Header />
       <PadInput />
-      <DesignBoard />
       <div className='flex-container flex-column' id="pad-container">
-        <div className='flex-container flex-wrap' id="pad-buttons">
-          <PadButton />
-          <PadButton />
-          <PadButton />
-          <PadButton />
-        </div>
+          <DesignBoard/>
       </div>
     </div>
   )
