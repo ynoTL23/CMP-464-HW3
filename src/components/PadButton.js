c0e43f7f2697f0c29713ace5562881c9b672b123
ynoTL <tonylu23@gamil.com>
2021-04-25 18:45:21 -0400

    create PadButton component

--- src/components/PadButton.js
+++ src/components/PadButton.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../styles/PadButton.css'
 
-const PadButton = () => {
-  return <h1>DesignPad</h1>
+const PadButton = props => {
+  return (
+    <div className='PadButton'>
+      <div className='PadButton-text'>{props.text}</div>
+    </div>
+  )
 }
 
 export default PadButton
